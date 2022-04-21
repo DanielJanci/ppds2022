90c2d188785e9075d6879f49eb88a4d3cd66f81c
DanielJanci <DanielJanci@users.noreply.github.com>
2022-04-21 17:38:51 +0200

    style: add docstrings in mainasync.py

--- mainasync.py
+++ mainasync.py
@@ -7,12 +7,23 @@ import time
 
 
 async def task(n, ident):
+    """
+    Waits for 0.3 seconds and then prints this tasks' id and hom many times it has been called.
+    :param n: number of iterations
+    :param ident: identificator of task
+    :return: None
+    """
     for i in range(n):
         await asyncio.sleep(0.3)
         print(f'task id: {ident}, times called: {i + 1}')
 
 
 def create_tasks(arr):
+    """
+    Creates instance of function task for each integer in list.
+    :param arr: list of integers
+    :return: list of tasks
+    """
     tasks = []
     for i in range(len(arr)):
         tasks.append(task(arr[i], i))
@@ -20,11 +31,20 @@ def create_tasks(arr):
 
 
 async def scheduler(arr):
+    """
+    Creates tasks from list. Runs multiple tasks using asyncio.gather.
+    :param arr: list of integers
+    :return: None
+    """
     tasks = create_tasks(arr)
     await asyncio.gather(*tasks)
 
 
 def main():
+    """
+    Creates an list of itegers and calls asyncio.run on function "scheduler" with that list as a parameter.
+    :return:
+    """
     arr = [4, 4, 4, 5, 3]
     start = time.perf_counter()
     asyncio.run(scheduler(arr))
